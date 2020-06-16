aa20e2d9cee62625d247a4cf1be3da6ddb18433f
Yize Chen <yizechen@uw.edu>
2020-06-16 15:24:46 -0700

    Update Construct_mobility.py

--- Data/Construct_mobility.py
+++ Data/Construct_mobility.py
@@ -1,24 +1,32 @@
+#Construct mobility dataset based on data from Google and Apple
+
 import csv
 import numpy as np
 import pandas as pd
 
-df1=pd.read_csv('Google/FR_Occitanie.csv', header=None)
-df2=pd.read_csv('Apple/Toulouse.csv', header=None).T
-df4=df2[34:34+91]
 
+#Change the file name to specific mobility data
+Goog_loc='Google/Seattle.csv'
+Apple_loc='Apple/Seattle.csv'
+Location='Seattle'
+
+df1=pd.read_csv('%s'%Goog_loc, header=None)
+df2=pd.read_csv('%s'%Apple_loc, header=None).T
+
+#The date shift of apple data and google data based on the their report updates
+start_index=34
+end_index=34+91
 
 goog_data=df1.values
-apple_data=df4.values
+apple_data=df2.values
 
-print(np.shape(goog_data))
-print(np.shape(apple_data))
 
-Mobi_data=np.concatenate((apple_data, goog_data[:91]), axis=1)
+Mobi_data=np.concatenate((apple_data, goog_data[start_index:end_index]), axis=1)
 
 
 
 
-with open('Toulouse_Mobility.csv', 'wb') as f:
+with open('%s_Mobility.csv'%Location, 'wb') as f:
     writer = csv.writer(f)
     writer.writerows(Mobi_data)
 
