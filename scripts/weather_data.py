070b13045186f04fe528eb18eaa12f8d35ef3166
Yize Chen <yizechen@uw.edu>
2020-06-16 15:37:20 -0700

    Update weather_data.py

--- scripts/weather_data.py
+++ scripts/weather_data.py
@@ -1,3 +1,6 @@
+#Download the Weather data for specific location
+#For instructions and API, check https://pypi.org/project/wwo-hist/
+
 import numpy as np
 from wwo_hist import retrieve_hist_data
 from six.moves.urllib.parse import urlparse
@@ -7,8 +10,8 @@ from urlparse import urlparse
 frequency=1
 start_date = '1-Jan-2018'
 end_date = '15-May-2020'
-api_key = 'ed92deaeb09b4a128d855844200505'
-location_list = ['Houston','Dallas','78205']
+api_key = 'your key' #Change to the key you got from the API
+location_list = ['Houston','Dallas','78205'] #Change to the location of interests
 
 hist_weather_data = retrieve_hist_data(api_key,
                                 location_list,
